a1ff4e2155ade9d57b4a98d261aab7785d537286
Angel <gutierrezramosjoseangel@gmail.com>
2021-12-15 15:59:34 -0600

    Lazyload with React router v5, Suspense and chunking name changed

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,32 +1,54 @@
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route, NavLink, Navigate } from "react-router-dom";
+import { Suspense } from "react";
+import { 
+	BrowserRouter as Router, 
+	Switch, 
+	Route, 
+	NavLink, 
+	Redirect
+} from "react-router-dom";
+
 import logo from '../logo.svg';
+import { routes } from "./routes";
 
 export const Navigation = () => {
     return (
-        <BrowserRouter>
-            <div className="main-layout">
-				<nav>
-					<img src={logo} alt="React Logo" />
-					<ul>
-						<li>
-							<NavLink to="/home" className={({isActive}) => isActive ? 'nav-active' : ''}>Home</NavLink>
-						</li>
-						<li>
-							<NavLink to="/about" className={({isActive}) => isActive ? 'nav-active' : ''}>About</NavLink>
-						</li>
-						<li>
-							<NavLink to="/users" className={({isActive}) => isActive ? 'nav-active' : ''}>Users</NavLink>
-						</li>
-					</ul>
-				</nav>
-				<Routes>
-					<Route path="about" element={<h1>About Page</h1>} />
-					<Route path="users" element={<h1>Users Page</h1>} />
-					<Route path="home" element={<h1>Home Page</h1>} />
-					<Route path="/*" element={<Navigate to="/home"  replace />} />
-				</Routes>
-			</div>
-        </BrowserRouter>
+		<Suspense fallback={<span>Loading...</span>}>
+			<Router>
+				<div className="main-layout">
+					<nav>
+						<img src={logo} alt="React Logo" />
+						<ul>
+							{
+								routes.map(({path, name}) => (
+									<li key={path}>
+										<NavLink 
+											to={path} 
+											activeClassName="nav-active"
+											exact
+										>
+											{name}
+										</NavLink>
+									</li>
+								))
+							}
+						</ul>
+					</nav>
+					{/* A <Switch> looks through its children <Route>s and
+					renders the first one that matches the current URL. */}
+					<Switch>
+						{
+							routes.map(({path, component: Component}) => (
+								<Route 
+									key={path}
+									path={path} 
+									render={() => <Component />} 
+								/>
+							))
+						}
+						<Redirect to={routes[0].path} />
+					</Switch>
+				</div>
+			</Router>
+		</Suspense>
     )
 }

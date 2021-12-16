a5e66d3f7f25c141363b31a4e12750084ec0688d
Angel <gutierrezramosjoseangel@gmail.com>
2021-12-16 01:00:24 -0600

    Component Compound Added children

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,9 +1,11 @@
+import { ReactElement } from 'react';
 import { useProduct } from '../hooks/useProduct';
 import styles from '../styles/styles.module.css';
 import noImage from '../assets/no-image.jpg';
 
 interface Props {
-    product: Product
+    product: Product;
+    children?: ReactElement | ReactElement[];
 }
 
 interface Product {
@@ -45,17 +47,18 @@ export const ProductButtons = ({ counter, increaseBy }: ProductButtonProps) => {
     )
 }
 
-export const ProductCard = ({ product }: Props) => {
+export const ProductCard = ({ children, product }: Props) => {
     const { counter, increaseBy } = useProduct();
 
     return (
         <div className={ styles.productCard }>
-            <ProductImage img={ product.img } />
+            { children }
+            {/* <ProductImage img={ product.img } />
             <ProductTitle title={ product.title} />
             <ProductButtons 
                 counter={ counter } 
                 increaseBy={ increaseBy } 
-            />
+            /> */}
         </div>
     )
 }
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,4 @@
-import { ProductCard } from "../components/ProductCard";
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components/ProductCard";
 
 const product = {
     id: '1',
@@ -16,7 +16,13 @@ export const ShoppingPage = () => {
                 flexDirection: 'row',
                 flexWrap: 'wrap'
             }}>
-                <ProductCard product={ product }/>
+                <ProductCard product={ product }>
+                    <ProductImage />
+                    <ProductTitle title={ "" } />
+                    <ProductButtons counter={0} increaseBy={function (value: number): void {
+                        throw new Error("Function not implemented.");
+                    } } />
+                </ProductCard>
             </div>
         </div>
     )

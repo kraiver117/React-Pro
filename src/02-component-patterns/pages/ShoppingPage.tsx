f6a2ee3129fbfbde35d152a95847167c099c9ca6
Angel <gutierrezramosjoseangel@gmail.com>
2021-12-16 16:56:58 -0600

    ClassName added on ProductButtons

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -17,19 +17,22 @@ export const ShoppingPage = () => {
                 flexDirection: 'row',
                 flexWrap: 'wrap'
             }}>
-                <ProductCard product={ product }>
+                <ProductCard 
+                    product={ product }
+                    className='bg-dark text-white'
+                >
                     <ProductCard.Image />
                     <ProductCard.Title title={ 'Hola mundo' } />
-                    <ProductCard.Buttons />
+                    <ProductCard.Buttons className='custom-buttons' />
                 </ProductCard>
 
                 <ProductCard 
                     product={ product }
-                    className='bg-dark'
+                    className='bg-dark text-white'
                 >
-                    <ProductImage className="custom-image" />
-                    <ProductTitle className='text-white text-bold' />
-                    <ProductButtons />
+                    <ProductImage className='custom-image' />
+                    <ProductTitle className='text-bold' />
+                    <ProductButtons className='custom-buttons' />
                 </ProductCard>
             </div>
         </div>

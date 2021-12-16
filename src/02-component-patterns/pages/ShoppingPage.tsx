06f5d4936fafbb9e96e8bdb91479aea3dbfeb9ba
Angel <gutierrezramosjoseangel@gmail.com>
2021-12-16 02:00:10 -0600

    Created files for each component and interface

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,4 @@
-import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components/ProductCard";
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components";
 
 const product = {
     id: '1',
@@ -16,15 +16,15 @@ export const ShoppingPage = () => {
                 flexDirection: 'row',
                 flexWrap: 'wrap'
             }}>
-                <ProductCard product={ product }>
+                {/* <ProductCard product={ product }>
                     <ProductCard.Image />
                     <ProductCard.Title title={ 'Hola mundo' } />
                     <ProductCard.Buttons />
-                </ProductCard>
+                </ProductCard> */}
 
                 <ProductCard product={ product }>
                     <ProductImage />
-                    <ProductTitle />
+                    <ProductTitle title="Cafe" />
                     <ProductButtons />
                 </ProductCard>
             </div>

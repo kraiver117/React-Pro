51be6b3f8553aa3efe400a196e653f2a20cfa072
Angel <gutierrezramosjoseangel@gmail.com>
2021-12-16 00:22:36 -0600

    Receive props on children component

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,11 +1,23 @@
 import { ProductCard } from "../components/ProductCard";
 
+const product = {
+    id: '1',
+    title: 'Coffe Mug - 1',
+    img: './coffee-mug.png'
+}
+
 export const ShoppingPage = () => {
     return (
         <div>
             <h1>Shopping Store</h1>
             <hr />
-            <ProductCard />
+            <div style={{ 
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap'
+            }}>
+                <ProductCard product={ product }/>
+            </div>
         </div>
     )
 }

972933501cf0a574660923276e9ebd51c67cd5ff
Angel <gutierrezramosjoseangel@gmail.com>
2021-12-16 00:11:51 -0600

    Basic component created & Custom hook implemented

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,32 +1,37 @@
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route, NavLink, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 import logo from '../logo.svg';
+import { ShoppingPage } from '../02-component-patterns/pages/ShoppingPage';
 
 export const Navigation = () => {
     return (
-        <BrowserRouter>
+        <Router>
             <div className="main-layout">
 				<nav>
 					<img src={logo} alt="React Logo" />
 					<ul>
 						<li>
-							<NavLink to="/home" className={({isActive}) => isActive ? 'nav-active' : ''}>Home</NavLink>
+							<NavLink to="/" activeClassName="nav-active" exact>Shopping</NavLink>
 						</li>
 						<li>
-							<NavLink to="/about" className={({isActive}) => isActive ? 'nav-active' : ''}>About</NavLink>
+							<NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
 						</li>
 						<li>
-							<NavLink to="/users" className={({isActive}) => isActive ? 'nav-active' : ''}>Users</NavLink>
+							<NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
 						</li>
 					</ul>
 				</nav>
-				<Routes>
-					<Route path="about" element={<h1>About Page</h1>} />
-					<Route path="users" element={<h1>Users Page</h1>} />
-					<Route path="home" element={<h1>Home Page</h1>} />
-					<Route path="/*" element={<Navigate to="/home"  replace />} />
-				</Routes>
+				<Switch>
+					<Route path="/about">
+						<h1>About</h1>
+					</Route>
+					<Route path="/users">
+						<h1>Users</h1>
+					</Route>
+					<Route path="/">
+						<ShoppingPage />
+					</Route>
+				</Switch>
 			</div>
-        </BrowserRouter>
+        </Router>
     )
 }

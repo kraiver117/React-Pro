25df366f569d218374f1f452d25f97d5956dd89f
Angel <gutierrezramosjoseangel@gmail.com>
2021-12-17 15:57:09 -0600

    Created Shopping Cart

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -25,7 +25,17 @@ export const ShoppingPage = () => {
     const [shoppingCart, setShoppingCart] = useState<{[key: string]: ProductInCart}>({});
 
     const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
-        console.log('onProductCountChange',count, product);
+        setShoppingCart(oldShoppingCart => {
+            if (count === 0) {
+                const { [product.id]: toDelete, ...rest } = oldShoppingCart;    
+                return rest;
+            }
+
+            return {
+                ...oldShoppingCart,
+                [product.id]: { ...product, count }
+            };
+        });
     }
 
     return (
@@ -69,6 +79,11 @@ export const ShoppingPage = () => {
                     <ProductImage className='custom-image' />
                     <ProductButtons className='custom-buttons' />
                 </ProductCard>
+            </div>
+            <div>
+                <code>
+                    { JSON.stringify(shoppingCart, null, 5) }
+                </code>
             </div>
         </div>
     )
